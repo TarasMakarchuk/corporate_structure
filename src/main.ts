415d1ca69318ce4dc643a6dd28ab17d4092f8f02
Taras Makarchuk <2278452@gmail.com>
2023-02-27 22:03:06 +0200

    :bookmark: add validation pipe, class validator and class transformer npms

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
-import * as fs from "fs";
-import * as morgan from "morgan";
+import { Logger, ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
+import * as morgan from 'morgan';
 import { AppModule } from './app.module';
 
 const logStream = fs.createWriteStream('api.log', {
@@ -14,6 +14,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
   app.use(morgan('tiny', { stream: logStream }));
   await app.listen(PORT);
   Logger.log(`Server start on host http://localhost:${PORT}`);
--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,6 +1,10 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserDto {
   readonly firstName: string;
   readonly lastName: string;
+  @IsEmail()
   readonly email: string;
+  @IsString()
   readonly password: string;
 }
